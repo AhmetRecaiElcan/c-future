c39caca06ae441f6bdb5dafbe1274e93a9d56f2f
ahmetrecaiellcan@gmail.com <ahmetrecaiellcan@gmail.com>
2023-07-01 00:10:05 +0300

    30.06.2023

--- test5(for döngüsü)/main.c
+++ test5(for döngüsü)/main.c
@@ -3,5 +3,18 @@
 
 int main()
 {
+    int sayi;
+    int i;
+    int toplam=0 ;
+
+    printf("Lutfen bir sayi giriniz\n");
+    scanf("%d",&sayi);
     
+
+    for (i =1 ; i<=sayi ; i++)
+    {
+        toplam=i+toplam;
+    }
+    printf("Toplam:%d",toplam);
 }
+/*bu zordu ya girdi*/
