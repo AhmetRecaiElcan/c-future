78102e5f339487f45d93bb312c7e93d45bb5c9ed
ahmetrecaiellcan@gmail.com <ahmetrecaiellcan@gmail.com>
2023-07-08 10:13:50 +0300

    08.07.2023

---
+++ test10(casting ve asci)/main2.c
@@ -0,0 +1,14 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    int i;
+    for ( i = 'a'; i <='z'; i++)
+    {
+        printf("%c\n",i);
+    }
+    
+}
---
+++ test11(diziler)/main.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+int main()
+{
+    int i;
+    int can[10];
+    for ( i = 0; i < 10; i++)
+    {
+        can[i]=2*i;        
+    }
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d\n",can[i]);
+    }
+  /*Çok sertti beyin yakar müsait vakitlerde buna bak*/  
+    
+    
+}
